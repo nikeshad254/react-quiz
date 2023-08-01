cfb122c6c5b72df0b9893cd5166d518f8d76b914
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-08-01 19:57:44 +0545

    react quiz with fake api and useReducer Hook

---
+++ src/components/Progress.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Progress = ({index, numQuestions, points, maxPossiblePoints, answer}) => {
+  return (
+    <header className='progress'>
+    
+        <progress max={numQuestions} value={index + Number(answer !== null)}/>
+
+      <p>Question <strong>{index + 1}</strong> / {numQuestions} </p>
+      <p>star <strong>{points}</strong> / {maxPossiblePoints} </p>
+    </header>
+  )
+}
+
+export default Progress
